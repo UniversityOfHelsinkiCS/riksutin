ee168afb56b9ed38e913318bb5c6f65f2e9314c3
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-03-03 12:37:51 +0200

    Add missing FULL_URL to config

--- config/index.ts
+++ config/index.ts
@@ -11,6 +11,7 @@ export const OIDC_ISSUER = inProduction
 
 export const PRODUCTION_URL = 'https://risk-i.helsinki.fi'
 export const STAGING_URL = 'https://riksutin.ext.ocp-test-0.k8s.it.helsinki.fi'
+export const DEVELOPMENT_URL = 'http://localhost:3000'
 
 export const API_TOKEN = process.env.API_TOKEN ?? ''
 export const JAMI_URL = inProduction
@@ -20,6 +21,9 @@ export const PATE_URL = inProduction
   ? 'https://api-toska.apps.ocp-prod-0.k8s.it.helsinki.fi/pate/'
   : 'https://api-toska.apps.ocp-test-0.k8s.it.helsinki.fi/pate/'
 
+// eslint-disable-next-line no-nested-ternary
+const FULL_URL = inProduction ? PRODUCTION_URL : inStaging ? STAGING_URL : DEVELOPMENT_URL
+
 // EXTERNAL
 import type { InstanceExports } from '@config'
-export default {} as Partial<InstanceExports>
+export default { FULL_URL } as Partial<InstanceExports>
