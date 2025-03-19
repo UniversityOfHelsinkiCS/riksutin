f1f86e8f211d50f449291c9cd453ed342bc5741e
Veikkosuhonen <vesuvesuh@gmail.com>
2025-03-19 13:15:51 +0200

    fix: Dryrun enabled in prod

--- config/services/mailer.ts
+++ config/services/mailer.ts
@@ -72,6 +72,7 @@ const sendEmail = async (
   // Check if the email is being sent to a tester. If so, set dryrun to false
   const acuallySendInTesting = TESTER_EMAILS.some(email => emails.some(({ to }) => to === email))
   if (acuallySendInTesting) logger.info('Sending email to tester')
+  const acualSettings = acuallySendInTesting ? { ...settings, dryrun: false } : settings
 
   const mail = {
     template: {
@@ -79,7 +80,7 @@ const sendEmail = async (
       text,
     },
     emails,
-    settings: { ...settings, dryrun: !acuallySendInTesting },
+    settings: acualSettings,
   }
 
   await pateClient.post('/', mail)
