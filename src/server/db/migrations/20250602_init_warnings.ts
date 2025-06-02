7c48b870439e530d1b1bcfa824b78d64fc1a5907
ViLP357 <vilipi2009@gmail.com>
2025-06-02 16:26:32 +0300

    edited migration

--- src/server/db/migrations/20250602_init_warnings.ts
+++ src/server/db/migrations/20250602_init_warnings.ts
@@ -1,13 +1,8 @@
 import { DataTypes } from 'sequelize'
 
 import { Migration } from '../connection'
-// eslint-disable-next-line no-console
-console.log('WTF1')
 
 export const up: Migration = async ({ context: queryInterface }) => {
-  // eslint-disable-next-line no-console
-  console.log('WTF2')
-
   await queryInterface.createTable('warnings', {
     id: {
       type: DataTypes.STRING,
@@ -24,14 +19,6 @@ export const up: Migration = async ({ context: queryInterface }) => {
     },
     expiry_date: {
       type: DataTypes.DATE,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
     },
   })
 }
