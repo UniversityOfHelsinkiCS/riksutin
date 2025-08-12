8a77b4c5341ce4ecade794e74c9fd20cfbcaff60
Matti Luukkainen <mluukkai@iki.fi>
2025-08-12 13:31:13 +0300

    comment added

--- src/server/util/redis.ts
+++ src/server/util/redis.ts
@@ -26,6 +26,7 @@ export const get = async <T>(key: string): Promise<T | null> => {
 }
 
 export const setPermanent = async <T>(key: string, value: T) => {
+  // we are also saving the cached data to psql, just in case if something blows up...
   const existing = await Cache.findOne({ where: { key } })
   if (existing) {
     await existing.update({ value: value as unknown as object })
