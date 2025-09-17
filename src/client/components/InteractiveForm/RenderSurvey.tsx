216037862324e3eff3c10f0ba76a9b9970fe1062
Matti Luukkainen <mluukkai@iki.fi>
2025-09-17 18:00:42 +0300

    broke lint

--- src/client/components/InteractiveForm/RenderSurvey.tsx
+++ src/client/components/InteractiveForm/RenderSurvey.tsx
@@ -73,7 +73,6 @@ const RenderSurvey = ({
   const requiredFields = Object.keys(defaultValues).filter(value => !['1', '2', '7', 'faculty'].includes(value))
 
   // eslint-disable-next-line no-console
-  
   console.log('TOIMIIKO')
 
   return (
