d311f9d6301650add6b99302ded3be58c06e775d
outisa <savolainen.outi@helsinki.fi>
2025-10-17 17:17:22 +0300

    Fix stupidity

--- src/client/util/sentry.ts
+++ src/client/util/sentry.ts
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/browser'
 import { Integrations } from '@sentry/tracing'
-import { inProduction, inStaging, inE2EMode, SENTRY_DNS_FRONT, SENTRY_GIT_SHA } from '@config'
+import { inProduction, inStaging, inE2EMode, SENTRY_DNS, SENTRY_GIT_SHA } from '@config'
 
 const initializeSentry = () => {
   if (!inProduction || inStaging || inE2EMode) {
@@ -8,7 +8,7 @@ const initializeSentry = () => {
   }
 
   Sentry.init({
-    dsn: SENTRY_DNS_FRONT,
+    dsn: SENTRY_DNS,
     release: SENTRY_GIT_SHA,
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 1.0,
