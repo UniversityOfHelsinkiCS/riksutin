74730688c983bd2bf5384de832c26fce8cc51e6f
outisa <savolainen.outi@gmail.com>
2025-12-11 15:09:54 +0200

    Make even better naming

--- src/server/routes/myResearchApi.ts
+++ src/server/routes/myResearchApi.ts
@@ -2,14 +2,14 @@ import express from 'express'
 
 import { getRisksWithTuhatProject, getRiskWithTuhatProjectId } from '../services/myResearchApi'
 import { MyResearchData, Error } from '@server/types'
-import { HYAPI_TOKEN } from '@userconfig'
+import { HY_API_TOKEN } from '@userconfig'
 
 const myResearchApiRouter = express.Router()
 
 myResearchApiRouter.get<never, MyResearchData[] | Error, never, never>('/projects', async (req, res) => {
   const apiKey = req.get('api-key')
 
-  if (!apiKey || !HYAPI_TOKEN || apiKey !== HYAPI_TOKEN) {
+  if (!apiKey || !HY_API_TOKEN || apiKey !== HY_API_TOKEN) {
     return res.status(401).json({ error: 'unauthorised' })
   }
 
@@ -20,7 +20,7 @@ myResearchApiRouter.get<never, MyResearchData[] | Error, never, never>('/project
 
 myResearchApiRouter.get<never, MyResearchData | Error, never, never>('/projects/:projectUuid', async (req, res) => {
   const apiKey = req.get('api-key')
-  if (!apiKey || !HYAPI_TOKEN || apiKey !== HYAPI_TOKEN) {
+  if (!apiKey || !HY_API_TOKEN || apiKey !== HY_API_TOKEN) {
     return res.status(401).json({ error: 'unauthorised' })
   }
   const { projectUuid } = req.params
