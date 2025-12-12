638edb84588d2c0643aa3eeb90529f393d751f59
Matti Luukkainen <mluukkai@iki.fi>
2025-12-12 17:37:51 +0200

    Update countryRisks.ts

--- src/server/util/cron/countryRisks.ts
+++ src/server/util/cron/countryRisks.ts
@@ -1,8 +1,8 @@
 import logger from '../logger'
 import scheduleCronJob from './schedule'
-import { buildPerCountryCache } from '../../data/worldbank/util'
 import { cacheSanctionsData } from 'src/server/data/sanctions/sanctionsMap'
 import { cacheHdrData } from 'src/server/data/humanDevelopment'
+import { buildIndividualCountryCaches } from 'src/server/data/per_country_cache'
 
 export const buildCaches = async () => {
   logger.info('cache build started')
@@ -10,13 +10,13 @@ export const buildCaches = async () => {
   await cacheSanctionsData()
   logger.info('hdr cache')
   await cacheHdrData()
-  logger.info('Per country cache')
-  await buildPerCountryCache()
+  logger.info('Per country caches')
+  await buildIndividualCountryCaches()
   logger.info('caches built')
 }
 
 const startCountryRiskCron = () => {
-  const cronTime = '15 15 * * 5'
+  const cronTime = '15 18 * * 5'
   logger.info('Country risk data cron job scheduled')
   return scheduleCronJob(cronTime, buildCaches)
 }
