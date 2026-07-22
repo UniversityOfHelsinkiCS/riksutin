0eddae959964b5fd0062838f695d43db21c9c993
smxsm <74610181+e11-0a@users.noreply.github.com>
2026-07-22 13:14:51 +0300

    Fail admin check if no user is set

--- src/server/middleware/admin.ts
+++ src/server/middleware/admin.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response } from 'express'
 import UnauthorizedError from '../errors/UnauthorizedError'
 
 const adminHandler = (req, res: Response, next: NextFunction) => {
-  if (!req?.user.isAdmin) {
+  if (!req?.user?.isAdmin) {
     throw new UnauthorizedError('Unauthorized')
   }
 
