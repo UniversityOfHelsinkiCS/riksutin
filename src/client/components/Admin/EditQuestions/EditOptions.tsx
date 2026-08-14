f2d597b860842b8894b89bd6c2eaec1988e1335c
Markus <wahlman2002@gmail.com>
2026-08-14 13:43:25 +0300

    refactor: use react hook form for edit options

--- src/client/components/Admin/EditQuestions/EditOptions.tsx
+++ src/client/components/Admin/EditQuestions/EditOptions.tsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import MDEditor from '@uiw/react-md-editor'
 import { Box, Typography, Button } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import { enqueueSnackbar } from 'notistack'
+import { useForm, Controller, Control } from 'react-hook-form'
 
 import { ChoiceType, Locales, Question, OptionUpdates } from '@types'
 
@@ -11,52 +12,17 @@ import { useEditOptionMutation } from '../../../hooks/useOptionMutation'
 type Option<A> = A extends readonly (infer T)[] ? T : never
 
 const OptionItem = ({
+  language,
   option,
   optionNumber,
-  question,
-  language,
+  control,
 }: {
+  language: keyof Locales
   option: Option<ChoiceType>
   optionNumber: number
-  question: Question
-  language: keyof Locales
+  control: Control<OptionUpdates>
 }) => {
   const { t } = useTranslation()
-  const mutation = useEditOptionMutation(question.id, option.id)
-  const [optionTitle, setOptionTitle] = useState<string | undefined>(option.title[language])
-  const [optionData, setOptionData] = useState<string | undefined>('')
-
-  useEffect(() => {
-    if ('data' in option && !('text' in option)) {
-      setOptionData(option.data[language]) // Type narrow the multichoice type
-    }
-    setOptionTitle(option.title[language])
-  }, [language, option])
-
-  const handleSave = async () => {
-    let updatedOption: OptionUpdates = {
-      title: {
-        ...option.title,
-        [language]: optionTitle,
-      },
-    }
-    if ('data' in option && !('text' in option)) {
-      updatedOption = {
-        data: {
-          ...option.data,
-          [language]: optionData,
-        },
-        ...updatedOption,
-      }
-    }
-
-    try {
-      await mutation.mutateAsync(updatedOption)
-      enqueueSnackbar(t('admin:saveSuccess'), { variant: 'success' })
-    } catch (error: any) {
-      enqueueSnackbar(error.message, { variant: 'error' })
-    }
-  }
 
   return (
     <Box
@@ -76,22 +42,30 @@ const OptionItem = ({
           {t('admin:optionTitle', { optionNumber })}
           <Typography sx={{ ml: 1 }}>{language}</Typography>
         </Typography>
-        <MDEditor data-color-mode="light" height={200} value={optionTitle} onChange={setOptionTitle} />
+        <Controller
+          name={`title.${language}`}
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <MDEditor data-color-mode="light" height={200} value={value} onChange={onChange} />
+          )}
+        />
       </Box>
 
-      {optionData && (
+      {'data' in option && !('text' in option) && (
         <Box sx={{ mb: 2 }}>
           <Typography sx={{ display: 'flex', mb: 2 }} variant="h6">
             {t('admin:optionText', { optionNumber })}
             <Typography sx={{ ml: 1 }}>{language}</Typography>
           </Typography>
-          <MDEditor data-color-mode="light" height={400} value={optionData} onChange={setOptionData} />
+          <Controller
+            name={`data.${language}`}
+            control={control}
+            render={({ field: { value, onChange } }) => (
+              <MDEditor data-color-mode="light" height={400} value={value} onChange={onChange} />
+            )}
+          />
         </Box>
       )}
-
-      <Button variant="outlined" onClick={handleSave}>
-        {t('admin:save')}
-      </Button>
     </Box>
   )
 }
@@ -107,12 +81,50 @@ const EditOptions = ({
   question: Question
   language: keyof Locales
 }) => {
-  const { t: _ } = useTranslation()
+  const { t } = useTranslation()
+  const mutation = useEditOptionMutation(question.id, option.id)
+
+  const defaultValues: OptionUpdates = {
+    title: option.title,
+  }
+  if ('data' in option && !('text' in option)) {
+    defaultValues.data = option.data
+  }
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { isDirty },
+  } = useForm<OptionUpdates>({
+    defaultValues,
+  })
+
+  useEffect(() => {
+    reset(defaultValues)
+  }, [option, reset])
+
+  const onSubmit = async (data: OptionUpdates) => {
+    try {
+      await mutation.mutateAsync(data)
+      enqueueSnackbar(t('admin:saveSuccess'), { variant: 'success' })
+      reset(data)
+    } catch (error: any) {
+      enqueueSnackbar(error.message, { variant: 'error' })
+    }
+  }
 
   return (
-    <Box sx={{ mb: 2, display: 'flex' }}>
-      <OptionItem option={option} optionNumber={optionNumber} question={question} language={'fi'} />
-      <OptionItem option={option} optionNumber={optionNumber} question={question} language={language} />
+    <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+      <Box sx={{ mb: 2, display: 'flex' }}>
+        <OptionItem option={option} optionNumber={optionNumber} language="fi" control={control} />
+        <OptionItem option={option} optionNumber={optionNumber} language={language} control={control} />
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, mb: 4 }}>
+        <Button variant="contained" disabled={!isDirty} type="submit">
+          {t('admin:save')}
+        </Button>
+      </Box>
     </Box>
   )
 }
