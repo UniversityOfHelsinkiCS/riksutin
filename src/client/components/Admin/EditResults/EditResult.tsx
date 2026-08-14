45ec5fa94182884a04251c46ff5f09eb10abb4b1
Markus <wahlman2002@gmail.com>
2026-08-14 15:05:03 +0300

    refactor: use react hook form for edit results

--- src/client/components/Admin/EditResults/EditResult.tsx
+++ src/client/components/Admin/EditResults/EditResult.tsx
@@ -1,40 +1,15 @@
-import { useState, useEffect } from 'react'
 import MDEditor from '@uiw/react-md-editor'
 import { Box, Button } from '@mui/material'
 import { enqueueSnackbar } from 'notistack'
 import { useTranslation } from 'react-i18next'
+import { useForm, Controller, Control } from 'react-hook-form'
 
 import type { Locales, Result } from '@types'
+import { UpdatedResult } from '@validators/results'
 
 import { useEditResultMutation } from '../../../hooks/useResultMutation'
 
-const ResultItem = ({ language, result }: { language: keyof Locales; result: Result }) => {
-  const { t } = useTranslation()
-  const mutation = useEditResultMutation(result.id)
-
-  const { data, isSelected } = result
-
-  const [resultIsSelected, setResultIsSelected] = useState<any>('')
-
-  useEffect(() => {
-    setResultIsSelected(isSelected[language])
-  }, [language, isSelected])
-
-  const handleSave = async () => {
-    isSelected[language] = resultIsSelected
-
-    try {
-      await mutation.mutateAsync({ data, isSelected })
-      enqueueSnackbar(t('admin:saveSuccess'), { variant: 'success' })
-    } catch (error: any) {
-      enqueueSnackbar(error.message, { variant: 'error' })
-    }
-  }
-
-  if (!resultIsSelected) {
-    return null
-  }
-
+const ResultItem = ({ language, control }: { language: keyof Locales; control: Control<UpdatedResult> }) => {
   return (
     <Box
       sx={{
@@ -49,21 +24,60 @@ const ResultItem = ({ language, result }: { language: keyof Locales; result: Res
       }}
     >
       <Box sx={{ mb: 2 }}>
-        <MDEditor data-color-mode="light" height={200} value={resultIsSelected} onChange={setResultIsSelected} />
+        <Controller
+          name={`isSelected.${language}`}
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <MDEditor data-color-mode="light" height={200} value={value} onChange={onChange} />
+          )}
+        />
       </Box>
-
-      <Button variant="outlined" onClick={handleSave}>
-        {t('admin:save')}
-      </Button>
     </Box>
   )
 }
 
-const EditResult = ({ language, result }: { language: keyof Locales; result: Result }) => (
-  <Box sx={{ mb: 5, display: 'flex' }}>
-    <ResultItem language={'fi'} result={result} />
-    <ResultItem language={language} result={result} />
-  </Box>
-)
+const EditResult = ({ language, result }: { language: keyof Locales; result: Result }) => {
+  const { t } = useTranslation()
+  const mutation = useEditResultMutation(result.id)
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { isDirty },
+  } = useForm<UpdatedResult>({
+    values: {
+      isSelected: result.isSelected,
+      data: result.data,
+    } as UpdatedResult,
+    resetOptions: {
+      keepDirtyValues: true,
+    },
+  })
+
+  const onSubmit = async (data: UpdatedResult) => {
+    try {
+      await mutation.mutateAsync(data)
+      enqueueSnackbar(t('admin:saveSuccess'), { variant: 'success' })
+      reset(data)
+    } catch (error: any) {
+      enqueueSnackbar(error.message, { variant: 'error' })
+    }
+  }
+
+  return (
+    <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+      <Box sx={{ display: 'flex' }}>
+        <ResultItem language="fi" control={control} />
+        <ResultItem language={language} control={control} />
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, mb: 4 }}>
+        <Button variant="contained" disabled={!isDirty} type="submit">
+          {t('admin:save')}
+        </Button>
+      </Box>
+    </Box>
+  )
+}
 
 export default EditResult
